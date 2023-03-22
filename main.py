86fffcd769ed5c8931cb10d87178e08d3081a0d8
unknown <vishal.mane@everlytics.io>
2023-03-22 11:10:09 +0530

    linting

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import pickle
-#pickle files
-similarity = pickle.load(open("similarity.pickle","rb"))
-anime_names= pickle.load(open("name.pkl","rb"))
-#mongodb function
-def suggestion(i):
-    from pymongo import MongoClient
-    client = MongoClient(
-        "mongodb+srv://Vishal:Vishal22610@anime.7z766.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+from pymongo import MongoClient
+
+# pickle files
+similarity = pickle.load(open("similarity.pickle", "rb"))
+anime_names = pickle.load(open("name.pkl", "rb"))
+
+
+# connect to the database
+try:
+    client = MongoClient("mongodb+srv://Vishal:Vishal22610@anime.7z766.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     db = client.get_database('Anime')
     records = db.Anime
+except Exception as e:
+    print("Unable to connect to the database:", e)
+
+
+# mongodb function
+def suggestion(i):
+    # client = MongoClient(
+    #     "mongodb+srv://Vishal:Vishal22610@anime.7z766.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    # db = client.get_database('Anime')
+    # records = db.Anime
 
     myquery = ({'Name': i})
     mydoc = db.Anime.find(myquery)
@@ -25,20 +37,24 @@ def suggestion(i):
     rat = a.get('Rating')
     return name, img, score, syn, dur, stud, rat
 
-#home route
+
+# Flask App
 app = Flask(__name__)
+
+
+# Home Route
 @app.route("/", methods=["POST", "GET"])
-
 def hello_world():
-    return render_template("index_django.html",suggestions=anime_names)
+    return render_template("index_django.html", suggestions=anime_names)
 
-@app.route("/recommends", methods=["POST","GET"])
+
+@app.route("/recommends", methods=["POST", "GET"])
 def recommends():
     try:
         text = request.form["movie"]
-        names=similarity.get(text)
-        name1=names[0]
-        name2=names[1]
+        names = similarity.get(text)
+        name1 = names[0]
+        name2 = names[1]
         name3 = names[2]
         name4 = names[3]
         name5 = names[4]
@@ -50,12 +66,12 @@ def recommends():
         name4, img4, score4, syn4, dur4, stud4, rat4 = suggestion(name4)
         name5, img5, score5, syn5, dur5, stud5, rat5 = suggestion(name5)
 
-        return render_template('inner.html',suggestions=anime_names,img=img, name=name, score=score, dur=dur, rat=rat,syn=syn,
-                            stud=stud,name1=name1,score1=score1,stud1=stud1,rat1=rat1,dur1=dur1,img1=img1,
-                            name2=name2,score2=score2,stud2=stud2,rat2=rat2,dur2=dur2,img2=img2,
-                            name3=name3,score3=score3,stud3=stud3,rat3=rat3,dur3=dur3,img3=img3,
-                            name4=name4,score4=score4,stud4=stud4,rat4=rat4,dur4=dur4,img4=img4,
-                            name5=name5,score5=score5,stud5=stud5,rat5=rat5,dur5=dur5,img5=img5)
+        return render_template('inner.html', suggestions=anime_names, img=img, name=name, score=score, dur=dur, rat=rat, syn=syn,
+                                stud=stud, name1=name1, score1=score1, stud1=stud1, rat1=rat1, dur1=dur1, img1=img1,
+                                name2=name2, score2=score2, stud2=stud2, rat2=rat2, dur2=dur2, img2=img2,
+                                name3=name3, score3=score3, stud3=stud3, rat3=rat3, dur3=dur3, img3=img3,
+                                name4=name4, score4=score4, stud4=stud4, rat4=rat4, dur4=dur4, img4=img4,
+                                name5=name5, score5=score5, stud5=stud5, rat5=rat5, dur5=dur5, img5=img5)
     except TypeError:
         return "Anime Not Found"
 
@@ -77,5 +93,5 @@ def anime_info(anime_name):
                            name5=name5,score5=score5,stud5=stud5,rat5=rat5,dur5=dur5,img5=img5)
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=7860 ,debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860, debug=True)
